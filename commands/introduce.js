5e9abc72cf5afc19d9fcd39685736820b8d9e563
Visate <richardg887@me.com>
2016-12-05 21:03:26 +0000

    adding initial folders and files for commands and lists

---
+++ commands/introduce.js
@@ -0,0 +1,23 @@
+// Introduce command
+var Config = require("../config.json");
+
+exports.help = {
+  name: "introduce",
+  description: "Sends some information about myself",
+  extendedhelp: "Tells you a little bit of information about myself"
+};
+
+exports.config = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["intro", "info"],
+  permLevel: 0
+};
+
+exports.run = (bot, msg, suffix) => {
+  let msgArray = [];
+  msgArray.push(`Hello, I'm ${bot.user.username}! It's a pleasure to meet you ^-^`);
+  msgArray.push(`If you want to see what I can do, use \`${Config.prefix}\``);
+  msgArray.push(`If you have any questions, feel free to ask my creator Visate :blush:`);
+  msg.channel.sendMessage(msgArray);
+};
---
+++ lists/country_servers.js
@@ -0,0 +1,467 @@
+var countryServers = {
+  // Australia servers
+
+  "adelaide": {
+    name: "Adelaide",
+    invite: "https://discord.gg/0ZDr5SYHMyBDKbpe",
+    cooldown: 0
+  },
+
+  "brisbane": {
+    name: "Brisbane",
+    invite: "https://discord.gg/0ZD8LAL4hJvEYViS",
+    cooldown: 0
+  },
+
+  "cairns": {
+    name: "Cairns",
+    invite: "https://discord.gg/0eDrq3uuUSAeTK7s",
+    cooldown: 0
+  },
+
+  "canberra": {
+    name: "Canberra",
+    invite: "https://discord.gg/0ZfrQK7nk59RKjY9",
+    cooldown: 0
+  },
+
+  "darwin": {
+    name: "Darwin",
+    invite: "https://discord.gg/0j4lOe5NEJhmzZu8",
+    cooldown: 0
+  },
+
+  "melbourne": {
+    name: "Melbourne",
+    invite: "https://discord.gg/0ZD8H5WEzzfXs2tE",
+    cooldown: 0
+  },
+
+  "newcastle": {
+    name: "Newcastle",
+    invite: "https://discord.gg/0Zfqks79aldG43qh",
+    cooldown: 0
+  },
+
+  "perth": {
+    name: "Perth",
+    invite: "https://discord.gg/0ZRklMgwOCCXdxl8",
+    cooldown: 0
+  },
+
+  "port macquarie": {
+    name: "Port Macquarie",
+    invite: "https://discord.gg/0ZfqZJQHg86OFBNJ",
+    cooldown: 0
+  },
+
+  "sydney": {
+    name: "Sydney",
+    invite: "https://discord.gg/0ZBv6u3Iu4sR8Flo",
+    cooldown: 0
+  },
+
+  "tasmania": {
+    name: "Tasmania",
+    invite: "https://discord.gg/0ZU0Isybnb5N0sdU",
+    cooldown: 0
+  },
+
+  "townsville": {
+    name: "Townsville",
+    invite: "https://discord.gg/0exIe1ZoiENWAcRe",
+    cooldown: 0
+  },
+
+  // Country servers
+
+  "argentina": {
+    name: "Argentina",
+    invite: "https://discord.gg/0ZfreIeaue4ZWllZ",
+    cooldown: 0
+  },
+
+  "austria": {
+    name: "Austria",
+    invite: "https://discord.gg/0eDTvUmdTTnO34nD",
+    cooldown: 0
+  },
+
+  "azerbaijan": {
+    name: "Azerbaijan",
+    invite: "https://discord.gg/0eDU8koqZBua6zRi",
+    cooldown: 0
+  },
+
+  "belarus": {
+    name: "Belarus",
+    invite: "https://discord.gg/0eDkryprgZRSodAN",
+    cooldown: 0
+  },
+
+  "belgium": {
+    name: "Belgium",
+    invite: "https://discord.gg/0eDl7zo54KXDezAw",
+    cooldown: 0
+  },
+
+  "bolivia": {
+    name: "Bolivia",
+    invite: "https://discord.gg/0rAx4zNHesc6cG3i",
+    cooldown: 0
+  },
+
+  "brazil": {
+    name: "Brazil",
+    invite: "https://discord.gg/0eDQ03VPUfJgh34L",
+    cooldown: 0
+  },
+
+  "brunei": {
+    name: "Brunei",
+    invite: "https://discord.gg/0eDqt0PylwnNXvc6",
+    cooldown: 0
+  },
+
+  "bulgaria": {
+    name: "Bulgaria",
+    invite: "https://discord.gg/0eDqbBgXMJtpzNqk",
+    cooldown: 0
+  },
+
+  "cambodia": {
+    name: "Cambodia",
+    invite: "https://discord.gg/GVDgJhC",
+    cooldown: 0
+  },
+
+  "canada": {
+    name: "Canada",
+    invite: "https://discord.gg/0ZH4lwpdXig65iRs",
+    cooldown: 0
+  },
+
+  "chile": {
+    name: "Chile",
+    invite: "https://discord.gg/0is99YaNk3iCFaOg",
+    cooldown: 0
+  },
+
+  "china": {
+    name: "China",
+    invite: "https://discord.gg/0eDQD5NHxQfpV1ry",
+    cooldown: 0
+  },
+
+  "colombia": {
+    name: "Colombia",
+    invite: "https://discord.gg/0isAFUvc69ZDi5zv",
+    cooldown: 0
+  },
+
+  "croatia": {
+    name: "Croatia",
+    invite: "https://discord.gg/0eDqMTWZZxGFsQkm",
+    cooldown: 0
+  },
+
+  "cuba": {
+    name: "Cuba",
+    invite: "https://discord.gg/jECXWqf",
+    cooldown: 0
+  },
+
+  "czech republic": {
+    name: "Czech Republic",
+    invite: "https://discord.gg/0eDr4N0H87qO52wO",
+    cooldown: 0
+  },
+
+  "denmark": {
+    name: "Denmark",
+    invite: "https://discord.gg/0eDQQxxTUZV2WRfA",
+    cooldown: 0
+  },
+
+  "egypt": {
+    name: "Egypt",
+    invite: "https://discord.gg/0eDrJgBoZcnhpuBO",
+    cooldown: 0
+  },
+
+  "estonia": {
+    name: "Estonia",
+    invite: "https://discord.gg/0isANMe727dt6Slb",
+    cooldown: 0
+  },
+
+  "finland": {
+    name: "Finland",
+    invite: "https://discord.gg/0eDrUcKfwx2b2mtR",
+    cooldown: 0
+  },
+
+  "france": {
+    name: "France",
+    invite: "https://discord.gg/0ZfrmrAbjCCMK0qU",
+    cooldown: 0
+  },
+
+  "germany": {
+    name: "Germany",
+    invite: "https://discord.gg/0ZftW6tl70oFnyfN",
+    cooldown: 0
+  },
+
+  "greece": {
+    name: "Greece",
+    invite: "https://discord.gg/0eDQvBoidXPxVYV0",
+    cooldown: 0
+  },
+
+  "guam": {
+    name: "Guam",
+    invite: "https://discord.gg/0eDrgRlX8JMsSMJe",
+    cooldown: 0
+  },
+
+  "hong kong": {
+    name: "Hong Kong",
+    invite: "https://discord.gg/0eDQjbu1y5hqL9MP",
+    cooldown: 0
+  },
+
+  "hungary": {
+    name: "Hungary",
+    invite: "https://discord.gg/0eDs2YLWyKrWsoiI",
+    cooldown: 0
+  },
+
+  "india": {
+    name: "India",
+    invite: "https://discord.gg/0eDRr62KdHi5Ucjb",
+    cooldown: 0
+  },
+
+  "indonesia": {
+    name: "Indonesia",
+    invite: "https://discord.gg/0eDRVSi64xalGkJk",
+    cooldown: 0
+  },
+
+  "ireland": {
+    name: "Ireland",
+    invite: "https://discord.gg/0is83mA2NRSjC4Ct",
+    cooldown: 0
+  },
+
+  "israel": {
+    name: "Israel",
+    invite: "https://discord.gg/0bLHcYbMGWFoTmD8",
+    cooldown: 0
+  },
+
+  "italy": {
+    name: "Italy",
+    invite: "https://discord.gg/0eDRHRnaAC9AXWkd",
+    cooldown: 0
+  },
+
+  "japan": {
+    name: "Japan",
+    invite: "https://discord.gg/0dHiutjeu3hAK9Lu",
+    cooldown: 0
+  },
+
+  "kazakhstan": {
+    name: "Kazakhstan",
+    invite: "https://discord.gg/0eDsEUcr2CiwWtU9",
+    cooldown: 0
+  },
+
+  "lithuania": {
+    name: "Lithuania",
+    invite: "https://discord.gg/0isA3w9Pg1PCBJzu",
+    cooldown: 0
+  },
+
+  "malaysia": {
+    name: "Malaysia",
+    invite: "https://discord.gg/0eDRiiX06ruklt61",
+    cooldown: 0
+  },
+
+  "mexico": {
+    name: "Mexico",
+    invite: "https://discord.gg/0rAvVeMaiL45bNax",
+    cooldown: 0
+  },
+
+  "netherlands": {
+    name: "Netherlands",
+    invite: "https://discord.gg/0ZDe2a2hrXXNiRuY",
+    cooldown: 0
+  },
+
+  "new zealand": {
+    name: "New Zealand",
+    invite: "https://discord.gg/0dIAGN8v3Qjm6VKB",
+    cooldown: 0
+  },
+
+  "norway": {
+    name: "Norway",
+    invite: "https://discord.gg/0is8REcaCGQlMMnv",
+    cooldown: 0
+  },
+
+  "paraguay": {
+    name: "Paraguay",
+    invite: "https://discord.gg/0rAwcX1PfGFk9zwh",
+    cooldown: 0
+  },
+
+  "philippines": {
+    name: "Philippines",
+    invite: "https://discord.gg/0is9YPbrO5jEZWl6",
+    cooldown: 0
+  },
+
+  "poland": {
+    name: "Poland",
+    invite: "https://discord.gg/0aEnYYM2Eiv1vWIj",
+    cooldown: 0
+  },
+
+  "portugal": {
+    name: "Portugal",
+    invite: "https://discord.gg/0is9szbE6cDkq8Yn",
+    cooldown: 0
+  },
+
+  "puerto rico": {
+    name: "Puerto Rico",
+    invite: "https://discord.gg/0rNoDqv6j3hT14UH",
+    cooldown: 0
+  },
+
+  "romania": {
+    name: "Romania",
+    invite: "https://discord.gg/0isAY5pA7nPL59JU",
+    cooldown: 0
+  },
+
+  "russia": {
+    name: "Russia",
+    invite: "https://discord.gg/0b1KVm67Vmzy0Giv",
+    cooldown: 0
+  },
+
+  "serbia": {
+    name: "Serbia",
+    invite: "https://discord.gg/0dI9syWXyoRJ12Jn",
+    cooldown: 0
+  },
+
+  "singapore": {
+    name: "Singapore",
+    invite: "https://discord.gg/0dI9JQdkEAtjsSjl",
+    cooldown: 0
+  },
+
+  "slovakia": {
+    name: "Slovakia",
+    invite: "https://discord.gg/0dI8sDtu83gHKelq",
+    cooldown: 0
+  },
+
+  "south africa": {
+    name: "South Africa",
+    invite: "https://discord.gg/0rAw9ICnyIepqwSl",
+    cooldown: 0
+  },
+
+  "south korea": {
+    name: "South Korea",
+    invite: "https://discord.gg/0beSC9ID5k0l1WPt",
+    cooldown: 0
+  },
+
+  "spain": {
+    name: "Spain",
+    invite: "https://discord.gg/0b1JzGvEl0klFn1j",
+    cooldown: 0
+  },
+
+  "sweden": {
+    name: "Sweden",
+    invite: "https://discord.gg/0Zfrwm7kiUThImbK",
+    cooldown: 0
+  },
+
+  "switzerland": {
+    name: "Switzerland",
+    invite: "https://discord.gg/0b1JlqmGy0dCMp4m",
+    cooldown: 0
+  },
+
+  "taiwan": {
+    name: "Taiwan",
+    invite: "https://discord.gg/0b1JYAEdSI0hmwFf",
+    cooldown: 0
+  },
+
+  "thailand": {
+    name: "Thailand",
+    invite: "https://discord.gg/0hTNRzyTmIeh3bPw",
+    cooldown: 0
+  },
+
+  "turkey": {
+    name: "Turkey",
+    invite: "https://discord.gg/0b1JJlYzeLWFJU4E",
+    cooldown: 0
+  },
+
+  "uae": {
+    name: "UAE",
+    invite: "https://discord.gg/0b1JDsT2krXUctNY",
+    cooldown: 0
+  },
+
+  "uk": {
+    name: "UK",
+    invite: "https://discord.gg/0ZRKsDlRUCrMljdi",
+    cooldown: 0
+  },
+
+  "ukraine": {
+    name: "Ukraine",
+    invite: "https://discord.gg/0b1J65hvvp5REfvZ",
+    cooldown: 0
+  },
+
+  "uruguay": {
+    name: "Uruguay",
+    invite: "https://discord.gg/0b1IY52JEs8JRZAs",
+    cooldown: 0
+  },
+
+  "usa": {
+    name: "USA",
+    invite: "https://discord.gg/tmCtmxZ",
+    cooldown: 0
+  },
+
+  "venezuela": {
+    name: "Venezuela",
+    invite: "https://discord.gg/0b1GeMhOWFw0WOvh",
+    cooldown: 0
+  },
+
+  "vietnam": {
+    name: "Vietnam",
+    invite: "https://discord.gg/0Zyk8AMOT1Pzyy7E",
+    cooldown: 0
+  }
+};
