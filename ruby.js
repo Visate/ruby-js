8bb30f42f0647dbb3027bfde1dc2d1f8ad1d174e
Visate <richardg887@me.com>
2016-12-07 06:52:07 +0000

    changed command handling to support guild only, enabled tags in commands and to support direct messages better

--- ruby.js
+++ ruby.js
@@ -37,7 +37,6 @@ bot.readCmds = () => {
 
 bot.readCmds();
 
-
 bot.on("ready", () => {
   log(`Logged in as ${bot.user.username}!`);
   log(`Serving ${bot.users.size} users in ${bot.channels.size} channels of ${bot.guilds.size} servers.`);
@@ -69,6 +68,8 @@ bot.on("message", msg => {
   else if (bot.aliases.has(command)) cmd = bot.commands.get(bot.aliases.get(command));
 
   if (cmd) {
+    if (!cmd.config.enabled) return;
+    if (guildOnly && !message.guild) return;
     if (perms < cmd.config.permLevel) return;
     if ((!cmd.config.hasOwnProperty("alternateInvoke") || (cmd.config.hasOwnProperty("alternateInvoke") && !cmd.config.alternateInvoke)) && uPrefix === "!") return;
     cmd.run(bot, msg, suffix);
@@ -213,30 +214,31 @@ bot.reload = (command) => {
 
 bot.checkPerms = (msg) => {
   // reducing repetitiveness
-  let guild = msg.guild;
-  let member = msg.member;
-  let roles = guild.roles;
-  let topRole = member.highestRole;
-
   let permlvl = 0;
-  // roles to compare with
-  let trialMod = roles.find(role => role.name === "Trial Moderator");
-  let mod = roles.find(role => role.name === "Moderator");
-  let admin = roles.find(role => role.name === "Admin");
-  let superAdmin = roles.find(role => role.name === "Super Admin");
-  let trustedEmployee = roles.find(role => role.name === "Trusted Employee");
-  let ultimatePower = roles.find(role => role.name === "Ultimate Power");
+  let guild = msg.guild;
+  if (guild) {
+    let member = msg.member;
+    let roles = guild.roles;
+    let topRole = member.highestRole;
+    // roles to compare with
+    let trialMod = roles.find(role => role.name === "Trial Moderator");
+    let mod = roles.find(role => role.name === "Moderator");
+    let admin = roles.find(role => role.name === "Admin");
+    let superAdmin = roles.find(role => role.name === "Super Admin");
+    let trustedEmployee = roles.find(role => role.name === "Trusted Employee");
+    let ultimatePower = roles.find(role => role.name === "Ultimate Power");
 
-  // returns a permissions level that gets sent to the command handler
-  // largely done by comparisons with other roles
-  if (trialMod && trialMod.position <= topRole.position) permlvl = 1;
-  if (mod && mod.position <= topRole.position) permlvl = 2;
-  if (admin && admin.position <= topRole.position) permlvl = 3;
-  if (superAdmin && superAdmin.position <= topRole.position) permlvl = 4;
-  if (trustedEmployee && trustedEmployee.position <= topRole.position) permlvl = 5;
-  if (ultimatePower && ultimatePower.position <= topRole.position) permlvl = 6;
-  if (member.id === guild.ownerID) permlvl = 7;
-  if (config.permissions.master.includes(member.id)) permlvl = 8;
+    // returns a permissions level that gets sent to the command handler
+    // largely done by comparisons with other roles
+    if (trialMod && trialMod.position <= topRole.position) permlvl = 1;
+    if (mod && mod.position <= topRole.position) permlvl = 2;
+    if (admin && admin.position <= topRole.position) permlvl = 3;
+    if (superAdmin && superAdmin.position <= topRole.position) permlvl = 4;
+    if (trustedEmployee && trustedEmployee.position <= topRole.position) permlvl = 5;
+    if (ultimatePower && ultimatePower.position <= topRole.position) permlvl = 6;
+    if (member.id === guild.ownerID) permlvl = 7;
+  }
+  if (config.permissions.master.includes(msg.author.id)) permlvl = 8;
   return permlvl;
 };
 
