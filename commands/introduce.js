719c69650df0b65a60df06d884172b4f7554786a
Visate <richardg887@me.com>
2016-12-07 07:59:52 +0000

    added system status to msg

--- commands/introduce.js
+++ commands/introduce.js
@@ -3,14 +3,14 @@ var config = require("../config.json");
 
 exports.help = {
   name: "introduce",
-  description: "Sends some information about myself",
-  extendedhelp: "Tells you a little bit of information about myself"
+  description: "Sends some information about myself.",
+  extendedhelp: "Tells you a little bit of information about myself."
 };
 
 exports.config = {
   enabled: true,
   guildOnly: false,
-  aliases: ["intro"],
+  aliases: [],
   permLevel: 0
 };
 
@@ -19,5 +19,13 @@ exports.run = (bot, msg, suffix) => {
   msgArray.push(`Hello, I'm ${bot.user.username}! It's a pleasure to meet you ^-^`);
   msgArray.push(`If you want to see what I can do, use \`${config.settings.prefix}help\``);
   msgArray.push(`If you have any questions, feel free to ask my creator Visate :blush:`);
+  msgArray.push("```");
+  msgArray.push(`Statistics:`);
+  msgArray.push(`★ Mem Usage    : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`)
+  msgArray.push(`★ Uptime       : ${bot.uptime}`);
+  msgArray.push(`★ Users        : ${bot.users.size}`);
+  msgArray.push(`★ Servers      : ${bot.guilds.size}`);
+  msgArray.push(`★ Channels     : ${bot.channels.size}`);
+  msgArray.push("```");
   msg.channel.sendMessage(msgArray);
 };
