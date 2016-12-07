ece81eb0b684e3fd271ce35d007dd07b56dfa875
Visate <richardg887@me.com>
2016-12-07 06:42:31 +0000

    Initial commit

---
+++ commands/eval.js
@@ -0,0 +1,26 @@
+// Eval command
+var config = require("../config.json");
+
+exports.help = {
+  name: "eval",
+  usage: "<expression>",
+  description: "Runs arbitrary Javascript",
+  extendedhelp: "Runs arbitrary Javascript code and returns the output. Expression may contain multiple lines, reserved for bot master."
+};
+
+exports.config = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["ev", "debug"],
+  permLevel: 8
+};
+
+exports.run = (bot, msg, suffix) => {
+  try {
+    let evaled = eval(suffix);
+    if (typeof evaled !== "string") evaled = require('util').inspect(evaled);
+    msg.channel.sendMessage(`\`\`\`x1\n${bot.cleanText(evaled)}\n\`\`\``);
+  } catch (err) {
+    msg.channel.sendMessage(`\`\`\`x1\nError: ${bot.cleanText(err)}\n\`\`\``);
+  }
+};
