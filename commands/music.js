9934792fca5b4af599b2d2a2eaedc5a9d2af6a4e
Visate <richardg887@me.com>
2016-12-12 20:55:05 +0000

    initial music commands commit

--- commands/music.js
+++ commands/music.js
@@ -1,6 +1,24 @@
 // Music command
 const Discord = require("discord.js");
 const config = require("../config.json");
+const log = require("../scripts/log.js");
+
+// Command handler
+const commands = new Discord.Collection();
+const aliases = new Discord.Collection();
+fs.readdir("./music/", (err, files) => {
+  if (err) console.error(err);
+  log(`Loading a total of ${files.length} music commands...`);
+  files.forEach(f => {
+    if (f.substring(f.length - 3) !== ".js") return;
+    let command = require(`./music/${f}`);
+    log(`Loading music command: ${command.help.name}`);
+    commands.set(command.help.name, command);
+    command.config.aliases.forEach(alias => {
+      aliases.set(alias, command.help.name);
+    });
+  });
+});
 
 exports.help = {
   name: "music",
