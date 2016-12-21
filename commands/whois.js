9fbd9e4d41226e744cd0bf2963e2d2342a1689d0
Visate <richardg887@me.com>
2016-12-21 08:26:38 +0000

    completed whois command and helper script to create ids

--- commands/whois.js
+++ commands/whois.js
@@ -1,5 +1,8 @@
+// Whois command
 const moment = require("moment");
 const stripIndents = require("common-tags").stripIndents;
+const config = require("../config.json");
+const createID = require("../scripts/createid.js");
 
 exports.help = {
   name: "whois",
@@ -16,5 +19,96 @@ exports.config = {
 };
 
 exports.run = (bot, msg, suffix) => {
+  let memberList = msg.guild.members;
+  let members = [];
 
+  if (!suffix) members.push(msg.member);
+
+  else if (suffix) {
+
+    if (suffix.startsWith("<@!")) {
+      suffix = suffix.substring(3, suffix.length - 1);
+      msg.channel.sendMessage(`${msg.author}, you don't need to mention someone in order to use this command! Please avoid that next time~`);
+    }
+    else if (suffix.startsWith("<@")) {
+      suffix = suffix.substring(2, suffix.length - 1);
+      msg.channel.sendMessage(`${msg.author}, you don't need to mention someone in order to use this command! Please avoid that next time~`);
+    }
+
+    if (isNaN(suffix)) {
+      suffix = suffix.toLowerCase();
+      let names = memberList.filter(m => m.user.username.toLowerCase().includes(suffix));
+      let nicks = memberList.filter(m => m.nickname !== null && m.nickname.toLowerCase().includes(suffix));
+      let ids = [];
+
+      names.forEach(m => {
+        members.push(m);
+        ids.push(m.id);
+      });
+
+      nicks.forEach(m => {
+        if (ids.includes(m.id)) return;
+        members.push(m);
+      });
+    }
+
+    else members.push(memberList.find(m => m.id === suffix));
+  }
+
+  if (members.length > 1) {
+    let msgArray = [];
+    msgArray.push(`Multiple users were found with that search, run the command with \`${config.settings.prefix}whois <id>\``);
+    members.forEach(m => {
+      let userMsg = m.nickname ? `**${m.user.username} (${m.nickname}):** ${m.id}` : `**${m.user.username}:** ${m.id}`;
+      msgArray.push(userMsg);
+    });
+
+    msg.channel.sendMessage(msgArray);
+  }
+
+  else if (members.length === 1 && members[0] !== null && members[0] !== undefined) {
+    let member = members[0];
+    let randomID = createID();
+    let roles = "";
+    member.roles.forEach(r => {
+      if (r.name === '@everyone') return;
+      roles += `\`${r.name}\` `;
+    });
+    let details = stripIndents`
+    • User:                  \`${member.user.username}#${member.user.discriminator} ${member.nickname ? `(${member.nickname})` : ""}\`
+    • ID:                      \u200A${member.id}
+    • Roles:                \u2009${member.roles.size > 1 ? roles : "`@everyone`"}
+    • Date Joined:    \u2006${member.joinedAt.toUTCString()}
+    • Status:              \u2006${member.presence.status === "dnd" ? "Do Not Disturb" : member.presence.status.charAt(0).toUpperCase() + member.presence.status.slice(1)}
+    ${member.presence.game ? `${member.presence.game.streaming ? `• Streaming:       \u200A[${member.presence.game.name}](${member.presence.game.url})` : `• Playing:             ${member.presence.game.name}`}` : randomID}
+    \u200b
+    `;
+
+    details = details.replace(`\n${randomID}`, "");  // correct spacing if there is no game
+
+    let mesg = {
+      color: 3447003,
+      author: {
+        name: `${member.user.username}#${member.user.discriminator} (${member.id})`,
+        icon_url: member.user.avatarURL
+      },
+      fields: [
+        {
+          name: "❯ Member Details",
+          value: details
+        }
+      ],
+      thumbnail: {
+        url: member.user.avatarURL
+      },
+      footer: {
+        text: "Whois",
+        icon_url: bot.user.avatarURL
+      }
+    };
+
+    msg.channel.sendMessage("", { embed: mesg });
+  }
+
+  else msg.channel.sendMessage("No users were found with that search!");
 };
