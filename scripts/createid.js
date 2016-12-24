7666c3e9cb46fe239a8ac9ad1876456fb55c3f51
Visate <richardg887@me.com>
2016-12-23 22:48:13 -0800

    fixing formatting

--- scripts/createid.js
+++ scripts/createid.js
@@ -1,11 +1,11 @@
 // Returns a randomly created ID, default length of 10 can be overriden.
 
 module.exports = (length = 10) => {
-    let text = "";
-    let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let text = "";
+  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    for (let i = 0; i < length; i++)
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
+  for (let i = 0; i < length; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
 
-    return text;
-}
+  return text;
+};
