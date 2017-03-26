e7f62e2a8d04149f655904d7815854147432d3b6
Richard Gan <richard@designedbymajor.ca>
2017-03-25 23:15:09 -0700

    Updated forever script
    - removed old logger, replaced with generic log statements since I can't be bothered

--- run.js
+++ run.js
@@ -1,24 +1,22 @@
 const forever = require("forever-monitor");
-const log = require("./scripts/log.js");
-
 var child = new (forever.Monitor)("ruby.js", {
   args: []
 });
 
 child.on("restart", () => {
-  log("Restarting Ruby...");
+  console.log("Restarting Ruby...");
 });
 
 child.on("exit:code", (code) => {
-  log(`Ruby's process exited with code ${code}`);
+  console.log(`Ruby's process exited with code ${code}`);
   if (code === 21) {
-    log("Detected a shutdown code! Shutting down...");
+    console.log("Detected a shutdown code! Shutting down...");
     process.exit(0);
   }
 });
 
 child.on("exit", () => {
-  log("Ruby's process has stopped!");
+  console.log("Ruby's process has stopped!");
 });
 
 child.start();
