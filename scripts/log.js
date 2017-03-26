ab6b2197e6e8bb0b254d2a914d63e3b3b5ac2ba4
Richard Gan <richard@designedbymajor.ca>
2017-03-25 23:23:43 -0700

    Removed old scripts
    changed over to utils
    logging completely changed
    database completely changed

--- scripts/log.js
+++
@@ -1,7 +0,0 @@
-const moment = require("moment");
-const chalk = require("chalk");
-
-module.exports = (...args) => {
-  let now = moment().format("YYYY-MM-DD HH:mm:ss");
-  console.log(chalk.blue.bold(`[${now}]`), chalk.bold(...args));
-};
